cc723442ff6dc33957a16551c1f3c4acc27a4848
Darren Ethier <darren@roughsmootheng.in>
2015-06-16 14:34:19 -0400

    more changes to implement ajax js etc [touch: 8356]

--- EE_Multisite_Config.php
+++ EE_Multisite_Config.php
@@ -32,10 +32,12 @@ if ( !defined( 'EVENT_ESPRESSO_VERSION' ) ) {
 class EE_Multisite_Config extends EE_Config_Base {
 	public $delete_site_threshold;
 	public $delete_site_excludes;
+	public $delete_non_super_admin_users;
 
 	public function __construct() {
 		$this->delete_site_threshold = 30;
 		$this->delete_site_excludes = array( 1 );
+		$this->delete_non_super_admin_users = false;
 	}
 }
 
