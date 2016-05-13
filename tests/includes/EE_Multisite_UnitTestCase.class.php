69e97c614ba64d6552df504175f37fb27933dc39
Mike Nelson <michael@eventespresso.com>
2016-05-13 16:51:19 -0700

    [touch:9784]{t:40}fixed some more unit tests resulting of new blogs starting off as up-to-date instead of unsure

--- tests/includes/EE_Multisite_UnitTestCase.class.php
+++ tests/includes/EE_Multisite_UnitTestCase.class.php
@@ -21,21 +21,11 @@ class EE_Multisite_UnitTestCase extends EE_UnitTestCase {
 	protected function _create_a_blog_with_ee() {
 		global $wpdb;
 		$blog = $this->factory->blog->create_and_get();
-		//grab one
-		switch_to_blog( $blog->blog_id );
-		EE_Data_Migration_Manager::reset();
-		$this->assertTableDoesNotExist( $wpdb->prefix . "esp_attendee_meta" );
-		$this->assertTableExists( $wpdb->prefix . 'posts' );
-		$newest_dms_name = EE_Data_Migration_Manager::reset()->get_most_up_to_date_dms();
-		$newest_dms = EE_Registry::instance()->load_dms( $newest_dms_name );
 		//allow the creation of these tables, because we know they're temporary
 		remove_all_filters( 'FHEE__EEH_Activation__create_table__short_circuit' );
-		$newest_dms->schema_changes_before_migration();
+		EED_Multisite::switch_to_blog($blog->blog_id);
 		//and put the filters back in place
 		add_filter( 'FHEE__EEH_Activation__create_table__short_circuit', '__return_true' );
-		$this->assertTableExists( $wpdb->prefix . "esp_status" );
-		$this->assertEquals( 0, EEM_Status::reset()->count() );
-		EE_System::instance()->initialize_db_if_no_migrations_required();
 		$this->assertNotEquals( EE_Maintenance_Mode::level_2_complete_maintenance, EE_Maintenance_Mode::instance()->level() );
 		$this->assertTableExists( $wpdb->prefix . 'esp_attendee_meta' );
 		$this->assertNotEquals( 0, EEM_Status::reset()->count() );
@@ -47,7 +37,7 @@ class EE_Multisite_UnitTestCase extends EE_UnitTestCase {
 			'role' => 'administrator',
 		) );
 		update_user_meta( $admin->ID, 'primary_blog', get_current_blog_id() );
-		restore_current_blog();
+		EED_Multisite::restore_current_blog();
 
 		return EEM_Blog::instance()->get_one_by_ID( $blog->blog_id );
 	}
@@ -58,6 +48,8 @@ class EE_Multisite_UnitTestCase extends EE_UnitTestCase {
 	 * and resetting hooks (which are done by EE_UnitTestCase)
 	 */
 	protected function _pretend_ee_upgraded() {
+		//when EE gets upgraded, all the blogs stati are switched to "unsure"
+		EEM_Blog::instance()->mark_all_blogs_migration_status_as_unsure();
 		$this->_pretend_addon_hook_time();
 		EE_Register_Data_Migration_Scripts::register( 'Pretend_Upgrade', array(
 			'dms_paths' => array( EE_MULTISITE_PATH . 'tests/mocks/data_migration_scripts/' )
