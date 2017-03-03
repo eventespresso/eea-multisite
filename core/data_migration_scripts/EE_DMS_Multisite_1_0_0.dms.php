3b768267fb2cdb643fc5e8c22dc8cdba9c752488
Mike Nelson <michael@eventespresso.com>
2017-03-03 14:11:25 -0800

    [touch:10552]{t:10}actually remove the index on last_requested. It sounds good to me but I think darren may have said adding it somehow slowed the queries down significantly. Feel free to revert this to put it back in place if that wasnt the case

--- core/data_migration_scripts/EE_DMS_Multisite_1_0_0.dms.php
+++ core/data_migration_scripts/EE_DMS_Multisite_1_0_0.dms.php
@@ -39,8 +39,7 @@ class EE_DMS_Multisite_1_0_0 extends EE_Data_Migration_Script_Base {
 				BLG_last_requested datetime NOT NULL default '0000-00-00 00:00:00',
 				PRIMARY KEY  (BLM_ID),
 				KEY blog_id_fk (blog_id_fk),
-				KEY STS_ID (STS_ID),
-				KEY BLG_last_requested (BLG_last_requested)"
+				KEY STS_ID (STS_ID)"
 			);
 
 //			$this->insert_default_status_codes();
