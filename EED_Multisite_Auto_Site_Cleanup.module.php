3f6747e7c3cc4c606010da28da4220ab83259da2
Mike Nelson <michael@eventespresso.com>
2017-09-11 16:33:34 -0700

    [touch:11003]{t:60}schedules cron task, handles its callback, and fires actions for each interval, and during the last task actually archives the site. Theoretically. Untested

--- EED_Multisite_Auto_Site_Cleanup.module.php
+++ EED_Multisite_Auto_Site_Cleanup.module.php
@@ -63,7 +63,18 @@ class EED_Multisite_Auto_Site_Cleanup extends EED_Module
         if (is_multisite()) {
             //check for admin visits
             //setup cron task
+            add_filter(
+                'FHEE__EEH_Activation__get_cron_tasks',
+                array('EED_Multisite_Auto_Site_Cleanup','add_cron_task')
+            );
             //handle cron task callback
+            add_action(
+                'AHEE__EED_Multisite_Auto_Site_Cleanup__check_for_cleanup_tasks',
+                array(
+                    'EED_Multisite_Auto_Site_Cleanup',
+                    'check_for_cleanup_tasks'
+                )
+            );
             //redirect to splash if first visit in x months
         }
     }
@@ -112,6 +123,94 @@ class EED_Multisite_Auto_Site_Cleanup extends EED_Module
         );
     }
 
+
+
+    /**
+     * Adds a weekly cron task to check for site cleanup tasks
+     * @param $existing_cron_task_data
+     */
+    public static function add_cron_task($existing_cron_task_data)
+    {
+        $existing_cron_task_data['AHEE__EED_Multisite_Auto_Site_Cleanup__check_for_cleanup_tasks'] = 'weekly';
+        return $existing_cron_task_data;
+    }
+
+
+
+    /**
+     * Gets a list of time intervals when an action should take place.
+     * Keys are their labels, values are the time values associated with them
+     * @return array
+     */
+    public static function get_cleanup_task()
+    {
+        return array(
+                'first_warning' => '22 months',
+                'second_warning' => '23 months',
+                'bluff_archive' => '24 months',
+                'really_archive' => '25 months',
+            );
+    }
+
+
+
+    /**
+     * Gets the name used for the EE extra meta that records when the action
+     * for this interval was given
+     *
+     * @param $interval_label
+     * @return string
+     */
+    protected static function _get_action_record_name($interval_label)
+    {
+        return  sanitize_key($interval_label . '_event');
+    }
+    /**
+     * Checks for blogs that meet the criteria for cleanup tasks,
+     * and for each it finds, it fires a WP action with that cleanup task.
+     * When doing the last cleanup task, also archives the site.
+     */
+    public static function check_for_cleanup_tasks()
+    {
+        $previous_interval_label = null;
+        $intervals = EED_Multisite_Auto_Site_Cleanup::get_cleanup_task();
+        $last_interval = end($intervals);
+        reset($intervals);
+        foreach($intervals as $label => $interval) {
+            $date = date(EE_Datetime_Field::mysql_timestamp_format, strtotime('-' . $interval));
+            $query = array(
+                array(
+                    'BLG_last_admin_visit' => array('<', $date),
+                )
+            );
+            if($previous_interval_label !== null) {
+                $query[0] = array_merge(
+                    $query[0],
+                    array(
+                        'Extra_Meta.EXM_key' => EED_Multisite_Auto_Site_Cleanup::_get_action_record_name($previous_interval_label),
+                        'Extra_Meta.EXM_value' => array('IS_NOT_NULL')
+                    )
+                );
+            }
+            $blogs_matching_criteria = EEM_Blog::instance()->get_all($query);
+            foreach($blogs_matching_criteria as $blog) {
+                if($last_interval === $interval) {
+                    //it's the last interval. Cleanup time
+                    $blog->save(
+                        array(
+                        'archived' =>true
+                        )
+                    );
+                }
+                //record that it's been fired
+                $blog->add_extra_meta(EED_Multisite_Auto_Site_Cleanup::_get_action_record_name($label), current_time('mysql', true));
+                //fire an action other plugins can listen for
+                do_action('AHEE__EED_Multisite_Auto_Site_Cleanup', $blog, $label, $interval);
+            }
+            //remember this label during the next iteration
+            $previous_interval_label = $label;
+        }
+    }
 
 
 
