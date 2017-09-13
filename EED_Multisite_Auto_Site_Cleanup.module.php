14ea37aa1aae0a87163d8586229e7f1c8c44757d
Mike Nelson <michael@eventespresso.com>
2017-09-13 15:35:12 -0700

    [touch:11003]{t:15}needed to switch sites because although the blog table is global, the extra meta one isnt so it was trying to delete extra meta records from the subsites extra meta table not the main sites

--- EED_Multisite_Auto_Site_Cleanup.module.php
+++ EED_Multisite_Auto_Site_Cleanup.module.php
@@ -104,9 +104,11 @@ class EED_Multisite_Auto_Site_Cleanup extends EED_Module
             $threshold_time = strtotime('-22 months');
             if($last_visit < $threshold_time){
                 //ok forget we ever sent them any warnings etc
+                switch_to_blog(1);
                 foreach(EED_Multisite_Auto_Site_Cleanup::get_cleanup_tasks() as $label => $time_threshold) {
                     $current_blog->delete_extra_meta(EED_Multisite_Auto_Site_Cleanup::get_action_record_extra_meta_name($label));
                 }
+                restore_current_blog();
                 //tell them we won't be deleting their site anymore
                 EEH_Template::display_template(EE_MULTISITE_PATH . 'templates/multisite_site_archival_aborted.template.php');
                 die;
