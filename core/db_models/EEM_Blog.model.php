1b4ce036c5bfeeff713281d235f45b3d911d0f8f
Mike Nelson <michael@eventespresso.com>
2017-09-25 14:10:46 -0700

    [touch:11003]{t:20}BLG_last_requested and BLG_last_admin_visit are supposed to default to now, but providing current_time didnt do that (and doing it that way can have problems during long unit tests because that current_time is the time the model was constructed, not the time the object was constructed)

--- core/db_models/EEM_Blog.model.php
+++ core/db_models/EEM_Blog.model.php
@@ -82,8 +82,8 @@ class EEM_Blog extends EEM_Soft_Delete_Base
                 'BLM_ID'             => new EE_DB_Only_Int_Field('BLM_ID', __('Blog Meta ID', 'event_espresso'), false, 0),
                 'blog_id_fk'         => new EE_DB_Only_Int_Field('blog_id_fk', __('Blog ID', 'event_espresso'), false, 0),
                 'STS_ID'             => new EE_Foreign_Key_String_Field('STS_ID', __('Status', 'event_espresso'), false, self::status_unsure, 'Status'),
-                'BLG_last_requested' => new EE_Datetime_Field('BLG_last_requested', __('Last Request for this Blog', 'event_espresso'), false, current_time('timestamp')),
-                'BLG_last_admin_visit' => new EE_Datetime_Field('BLG_last_admin_visit', __('Last Request for this Blog by an Admin', 'event_espresso'), false, current_time('timestamp')),
+                'BLG_last_requested' => new EE_Datetime_Field('BLG_last_requested', __('Last Request for this Blog', 'event_espresso'), false, EE_Datetime_Field::now),
+                'BLG_last_admin_visit' => new EE_Datetime_Field('BLG_last_admin_visit', __('Last Request for this Blog by an Admin', 'event_espresso'), false, EE_Datetime_Field::now),
             ),
         );
         $this->_model_relations = array(
