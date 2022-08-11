ae423daf2e585727659f08cb896f6da677304edf
Brent Christensen <brent@eventespresso.com>
2022-08-11 16:55:59 -0700

    betterer dirname()

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -4,14 +4,14 @@
  */
 use EETests\bootstrap\AddonLoader;
 
-$core_tests_dir = dirname(dirname(dirname(__FILE__))) . '/event-espresso-core/tests/';
+$core_tests_dir = dirname(__FILE__, 3) . '/event-espresso-core/tests/';
 //if still don't have $core_tests_dir, then let's check tmp folder.
 if (! is_dir($core_tests_dir)) {
     $core_tests_dir = '/tmp/event-espresso-core/tests/';
 }
 require $core_tests_dir . 'includes/CoreLoader.php';
 require $core_tests_dir . 'includes/AddonLoader.php';
-define('EEA_MULTISITE_PLUGIN_DIR', dirname(dirname(__FILE__)) . '/');
+define('EEA_MULTISITE_PLUGIN_DIR', dirname(__FILE__, 2) . '/');
 define('EEA_MULTISITE_TESTS_DIR', EEA_MULTISITE_PLUGIN_DIR . 'tests/');
 $addon_loader = new AddonLoader(
     EEA_MULTISITE_TESTS_DIR,
